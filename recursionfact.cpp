e725cdc2ba377775e55527100f1d4d03bcfd2645
PRIYANSHA SHARMA <55697339+priyanshas9@users.noreply.github.com>
2019-10-04 20:41:38 +0530

    Update recursionfact.cpp

--- recursionfact.cpp
+++ recursionfact.cpp
@@ -1,17 +1,17 @@
 #include<iostream.h>
 #include<conio.h>
-long fact(int);
+long fact(int);   //Function declaration
 int main()
 {
 int n;
 int x;
 cout<<"enter the number";
 cin>>n;
-x=fact(n);
+x=fact(n);  //Function calling
 cout<<"the factorial of the number is"<<x;
 }
 /////////////////////////////////////////////////////////
-long fact(long n)//factorial funtion
+long fact(long n)//Function definition
 {
 if (n==0)
 return 1;
